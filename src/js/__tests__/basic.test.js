64b246d0007bd05891f421678d7b808eb30dc770
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-03-27 19:50:34 +0300

    done

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,2 +1,43 @@
-import  from "../app";
+import destructuringObj from "../app";
 
+test("show id, name, desciption and icons in obj", () => {
+  const character = {
+    name: "Лучник",
+    type: "Bowman",
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: "Двойной выстрел",
+        icon: "http://...",
+        description: "Двойной выстрел наносит двойной урон",
+      },
+      {
+        id: 9,
+        name: "Нокаутирующий удар",
+        icon: "http://...",
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const equal = [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      description: "описание не доступно",
+      // <- обратите внимание, описание "засекречено"
+    },
+  ];
+
+  expect(destructuringObj(character)).toEqual(equal);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 // TODO: write your code here
+export default function destructuringObj(data) {
+  const result = [];
+  for (const {
+    id,
+    name,
+    description = "описание не доступно",
+    icon,
+  } of data.special) {
+    result.push({
+      id,
+      name,
+      description,
+      icon,
+    });
+  }
+  return result;
+}
